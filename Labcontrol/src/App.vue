61c478b4839b666d12268285293c13ff24a20124
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-03-25 21:48:30 -0300

    Simple login form

--- Labcontrol/src/App.vue
+++ Labcontrol/src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
     <main>
-	 <div class="container">	    
-       <img src="./assets/logo.png" alt="Vue.js PWA">
        <router-view></router-view>
-	 </div>
     </main>
   </div>
 </template>
---
+++ Labcontrol/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-sm-12 col-lg-6">
+				<div class="card text-center" style="margin-top: 10%;"">
+					<div class="card-header">
+						Identifique-se, por favor
+					</div>
+					<div class="card-body">
+						<form>
+							<div class="form-group">
+								<label for="email">Email address</label>
+								<input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" id="password" placeholder="Password">
+							</div>
+							<button type="submit" class="btn btn-primary">Login</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
