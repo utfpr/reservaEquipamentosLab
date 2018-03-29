efd8139ae85bbfd9adbde19414a6506291ab87ed
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-03-29 13:10:44 -0300

    moving header and footer from index.html to App.vue

--- Labcontrol/src/App.vue
+++ Labcontrol/src/App.vue
@@ -1,9 +1,42 @@
 <template>
   <div id="app">
-    <main>
-       <router-view></router-view>
-    </main>
-  </div>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand text-justify" href="#">LabControl</a>
+
+        <div class="collapse navbar-collapse"  id="navbar">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer id="footer" class="footer" style="background-color: #DDDFE4;">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-auto">
+          <!-- <img src="./assets/logo_UTFPR.png" style="width:150px; margin:10%"> -->
+          <p class="text-center" style="font-size:1.2em; margin: 15px 0 15px 0">CAMPUS CAMPO MOURÃO - VIA ROSALINA MARIA DOS SANTOS, 1233 CEP 87301-899 Caixa Postal: 271 Campo Mourão - PR - Brasil<br>Telefone Geral +55 (44) 3518-1400</p>
+        </div>
+      </div>
+          </div>
+          <div class="copyright" style="background-color:#C9CBD0;">
+              <div class="container">
+                  <div class="row">
+        <small>&copy; Copyright 2018 <a href="http://portal.utfpr.edu.br/campus/campomourao" target="_blank" class="text-dark">DACOM UTFPR-CM</a> </small>
+                  </div>
+              </div>
+          </div>
+  </footer>
+</div>
 </template>
 
 <script>
@@ -12,6 +45,17 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
-   @import '../node_modules/bootstrap/dist/css/bootstrap.css'
+<style lang="css">
+   @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+   #app {
+     display: flex;
+     min-height: 100vh;
+     flex-direction: column;
+   }
+
+   main {
+     flex: 1 1 auto;
+   }
+
 </style>
--- Labcontrol/src/components/Home.vue
+++ Labcontrol/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
 	  <div class="container">
-		  
+      
 	  </div>
   </div>
 </template>
