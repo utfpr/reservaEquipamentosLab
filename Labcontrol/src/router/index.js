7de59dd7fddd9beaf58c7f6d91242b00a8f29d8a
vnc10 <viniciusbosa@gmail.com>
2018-03-30 22:26:05 -0300

    adicionado cadastro para ser acessado

--- Labcontrol/src/router/index.js
+++ Labcontrol/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
+import Cadastro from '@/components/Cadastro'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/Cadastro',
+      name: 'Cadastro',
+      component: Cadastro
     }
   ]
 })
