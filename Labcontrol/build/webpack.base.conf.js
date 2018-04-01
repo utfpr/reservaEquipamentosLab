a2c43f52cc5dc22596171e43e9a0de5feb7aa4b3
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-04-01 19:22:57 -0300

    remove unused loader

--- Labcontrol/build/webpack.base.conf.js
+++ Labcontrol/build/webpack.base.conf.js
@@ -71,14 +71,6 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      },
-      {
-	      test: /\.s[a|c]ss$/,
-	      loader: 'style!css!sass'
       }
     ]
   }
