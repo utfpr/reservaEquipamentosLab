c5e5937f3117e08453255f23fc26d3ba496afce9
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-04-01 20:44:29 -0300

    improving login layout

--- Labcontrol/src/components/Login.vue
+++ Labcontrol/src/components/Login.vue
@@ -14,7 +14,7 @@
                 <div class="input-group-prepend">
                   <div class="input-group-text d-xs-none d-md-flex"><i class="fas fa-envelope"></i></div>
                 </div>
-                <input type="email" v-model="email" class="form-control" id="email" placeholder="E-mail" @focus="" @blur="" >
+                <input type="email" v-model="email" class="form-control" id="email" placeholder="E-mail">
               </div>
             </div>
             <div class="form-group">
@@ -29,11 +29,11 @@
             <button class="btn btn-primary btn-block" v-on:click="login">Login</button>
           </form>
           <div class="row">
-            <div class="col-sm-6">
-              <a href="#" class="justify-content-start">Fazer cadastro</a>
+            <div class="col-sm-6 text-left">
+              <a href="#" class="text-link">Fazer cadastro</a>
             </div>
-            <div class="col-sm-6">
-              <a href="#" class="justify-content-end">Esqueci minha senha</a>
+            <div class="col-sm-6 text-right">
+              <a href="#" class="text-link">Esqueci minha senha</a>
             </div>
           </div>
         </div>
@@ -123,8 +123,16 @@ export default {
   display: none!important;
 }
 
+.text-link {
+  color: rgba(255, 255, 255, 0.5);
+}
+
+a:hover {
+  color: rgba(255, 255, 255, 0.5);
+}
+
 #login {
-  background-color: rgba(0,0,0,0.75);
+  background-color: rgba(0,0,0,0.65);
 }
 
 #login::before {
@@ -146,6 +154,77 @@ export default {
   -o-filter: blur(0.65em);
   -ms-filter: blur(0.65em);
   filter: blur(0.65em);
+}
+
+.input-group-text {
+  color: #fff;
+  background-color: transparent;
+  border: 0px;
+  border-bottom: 1px solid #fff;
+  border-radius: 0rem;
+}
+
+.form-control {
+  color: #fff;
+  border: 0px;
+  background-color: transparent;
+  background-clip: padding-box;
+  border-bottom: 1px solid #fff;
+  border-radius: 0rem;
+}
+
+.form-control:focus {
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0);
+  border: 0px;
+  border-bottom: 2px solid #fff;
+  outline: 0;
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+}
+
+.form-control::-webkit-input-placeholder {
+  color: #fff;
+  opacity: 1;
+}
+
+.form-control::-moz-placeholder {
+  color: #fff;
+  opacity: 1;
+}
+
+.form-control:-ms-input-placeholder {
+  color: #fff;
+  opacity: 1;
+}
+
+.form-control::-ms-input-placeholder {
+  color: #fff;
+  opacity: 1;
+}
+
+.form-control::placeholder {
+  color: #fff;
+  opacity: 1;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: rgba(44, 102, 206, 0.45);
+  border-color: rgba(0, 0, 0, 0);
+  border-radius: 0;
+}
+
+.btn-primary:hover {
+  color: #fff;
+  background-color: rgba(44, 102, 206, 0.75);
+  border-color: rgba(44, 102, 206, 0)
+}
+
+.btn-primary:focus, .btn-primary.focus {
+  color: #fff;
+  background-color: rgba(44, 102, 206, 0.75);
+  border-color: rgba(44, 102, 206, 0.75);
+  box-shadow: 0 0 0 0.2rem rgba(44, 102, 206, 0.5);
 }
 
 </style>
