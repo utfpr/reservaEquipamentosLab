b8715f82df93a84ae20ee9982192b4cc3e0d6802
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-03-31 21:46:27 -0300

    firebase auth

--- Labcontrol/src/components/Login.vue
+++ Labcontrol/src/components/Login.vue
@@ -1,75 +1,78 @@
 <template>
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-sm-12 col-md-8 col-lg-4 vertical-center">
-				<div class="card text-center" style="">
-					<div class="card-header">
-						Identifique-se, por favor
-					</div>
-					<div class="card-body">
-						<form id="login">
-							<div class="form-group">
-								<label class="sr-only" for="email">E-mail</label>
-								<div class="input-group ">
-								  <div class="input-group-prepend">
-								    <div class="input-group-text d-xs-none d-md-flex"><i class="fas fa-envelope"></i></div>
-								  </div>
-								<input type="email" class="form-control" id="email" placeholder="E-mail" @focus="focusForm('email')" @blur="blurForm()" >
-								</div>
-							</div>
-						     <div class="form-group">
-								<label class="sr-only" for="senha">Senha</label>
-							     <div class="input-group">
-							       <div class="input-group-prepend">
-							         <div class="input-group-text d-xs-none d-md-flex"><i class="fas fa-lock"></i></div>
-							       </div>
-							       <input type="password" class="form-control" id="senha" placeholder="Senha">
-							     </div>
-						     </div>
-						     <button type="submit" class="btn btn-primary">Login</button>
-						</form>
-					</div>
-					<div class="card-footer">
-						<div class="row">
-							<div class="col-sm-6">
-								<a href="#" class="justify-content-start">Fazer cadastro</a>
-							</div>
-							<div class="col-sm-6">
-								<a href="#" class="justify-content-end">Esqueci minha senha</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div id="login">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8 col-lg-4 vertical-center">
+          <div class="card text-center" style="">
+            <div class="card-header">
+              Identifique-se, por favor
+            </div>
+            <div class="card-body">
+              <form id="login">
+                <div class="form-group">
+                  <label class="sr-only" for="email">E-mail</label>
+                  <div class="input-group ">
+                    <div class="input-group-prepend">
+                      <div class="input-group-text d-xs-none d-md-flex"><i class="fas fa-envelope"></i></div>
+                    </div>
+                  <input type="email" v-model="email" class="form-control" id="email" placeholder="E-mail" @focus="" @blur="" >
+                  </div>
+                </div>
+                   <div class="form-group">
+                  <label class="sr-only" for="password">Senha</label>
+                     <div class="input-group">
+                       <div class="input-group-prepend">
+                         <div class="input-group-text d-xs-none d-md-flex"><i class="fas fa-lock"></i></div>
+                       </div>
+                       <input type="password" v-model="password" class="form-control" id="password" placeholder="Senha">
+                     </div>
+                   </div>
+                   <button class="btn btn-primary btn-block" v-on:click="login">Login</button>
+              </form>
+            </div>
+            <div class="card-footer">
+              <div class="row">
+                <div class="col-sm-6">
+                  <a href="#" class="justify-content-start">Fazer cadastro</a>
+                </div>
+                <div class="col-sm-6">
+                  <a href="#" class="justify-content-end">Esqueci minha senha</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'login',
   data () {
-    return {}
+    return {
+      email: '',
+      password: ''
+    }
   },
   methods: {
-    focusForm (id) {
-      let el = document.getElementById(id)
-      el.classList.add('focused')
-    },
-    blurForm () {
+    login: function () {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        (user) => {
+          this.$router.replace('/home')
+        },
+        (err) => {
+          alert('Oops ' + err.message)
+        }
+      )
     }
   }
 }
 </script>
 
 <style>
-.vertical-center {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
-.focused{
-     border-bottom-color: red;
-}
 </style>
