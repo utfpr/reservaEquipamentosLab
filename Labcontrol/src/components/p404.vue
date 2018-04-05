7f73967bdfcd5ab4e2beaf275fba12f1a61cce19
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-04-05 09:58:13 -0300

    improving 404 page

--- Labcontrol/src/components/p404.vue
+++ Labcontrol/src/components/p404.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="p404">
+    <div class="e404 vertical-center"></div>
     <div class="container">
-      <h2>404 AQUI</h2>
+      <div class="row justify-content-center">
+        <div class="home">
+          <router-link to="/home" class="btn btn-dark">Ir para Home</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,9 +17,78 @@ export default {
   data () {
     return {}
   },
+  mounted: function () {
+    let elements = document.getElementsByTagName('footer')
+    elements[0].classList.add('hideOn')
+  },
+  destroyed: function () {
+    let elements = document.getElementsByTagName('footer')
+    elements[0].classList.remove('hideOn')
+  },
   methods: {}
 }
 </script>
 
+
+
 <style>
+
+.p404::before {
+  content: "";
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: -1;
+
+  display: block;
+  background-position: center;
+  background-size:cover;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('../assets/img/404_back.svg');
+}
+
+
+@media (min-width: 0px) {
+  .e404 {
+    content: "";
+    position: fixed;
+    left: 0;
+    right: 0;
+    width: 100%;
+    background-image: url('../assets/img/404_front.svg');
+    background-size: cover;
+    background-position: center;
+    height: 0;
+    padding: 0; /* reset */
+    padding-bottom: 100%;
+  }
+
+  .home {
+    position: fixed;
+    top: 65%;
+  }
+}
+
+@media (min-width: 992px) {
+  .e404 {
+    position: fixed;
+    left: 0;
+    right: 0;
+    width: 100%;
+    background-image: url('../assets/img/404_front.svg');
+    background-size: cover;
+    background-position: center;
+    height: 0;
+    padding: 0; /* reset */
+    padding-bottom: 50%;
+  }
+
+  .home {
+    position: fixed;
+    top: 70%;
+  }
+
+}
+
 </style>
