3dce7765203d2b77371285431c4299e73cf169a2
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-06-03 20:19:50 -0300

    autocomplete changes

--- Labcontrol/src/components/equipamentos/EquipamentoDetails.vue
+++ Labcontrol/src/components/equipamentos/EquipamentoDetails.vue
@@ -57,9 +57,9 @@
                   <div class="input-group-prepend">
                     <span class="input-group-text" id="patrimonioPrepend"><i class="fas fa-list-ol"></i></span>
                   </div>
-                  <input v-on:focus="autocompleteHide()" v-on:change="checkUnique()" id="patrimonio" type="text" class="form-control" placeholder="Digite o numero de patrimonio" autocomplete="patrimonio" aria-describedby="patrimonioPrepend" min="0" v-model = "equipment.Patrimonio" required>
+                  <input v-on:focus="autocompleteHide()" v-on:change="checkUnique()" id="patrimonio" type="text" class="form-control" placeholder="Digite o patrimônio" autocomplete="patrimonio" aria-describedby="patrimonioPrepend" min="0" v-model = "equipment.Patrimonio" required>
                   <div class="invalid-feedback">
-                    Por favor informe um numero de patrimonio.
+                    Por favor informe um patrimônio válido e único.
                   </div>
                 </div>
               </div>
@@ -85,7 +85,9 @@
                   </div>
                   <input id="local" v-on:focus="autocompleteShow()" v-on:keyup="autocompleteSearch()" v-on:change="autocompleteHide()" type="text" class="form-control" placeholder="Local do equipamento" autocomplete="local" aria-describedby="localPrepend" v-model = "equipment.Local" required>
                   <div id="autocomplete" class="list-group autocomplete hideOn">
-                    <span v-for="local in locais" v-on:click="selectLocal(local)" class="list-group-item list-group-item-action">{{local}}</span>
+                    <li v-for="local in locais" v-on:click="selectLocal(local)" class="list-group-item list-group-item-action">
+                      <span v-on:click="selectLocal(local)">{{local}}</span>
+                    </li>
                   </div>
                   <div class="invalid-feedback">
                     Por favor informe um local válido.
