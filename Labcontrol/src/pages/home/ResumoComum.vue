9a0c2576c83c1df481e58d1239e4ae3a9ee659c5
Thais Zorawski <thaiszorawski@gmail.com>
2018-11-01 14:13:53 -0300

    Inclusão do ResumoComum na Home

--- Labcontrol/src/pages/home/ResumoComum.vue
+++ Labcontrol/src/pages/home/ResumoComum.vue
@@ -10,7 +10,7 @@
                     <div class="card text-white bg-primary mb-3 text-center vertical-center" style="max-width: 5rem; min-width: 5rem; max-height:5rem; min-height:5rem">
                       <div class="card-header" style="padding:0;">{{ month }}</div>
                       <div class="card-body" style="padding:0;">
-                        <h3>{{ day }}</h3>
+                        
                       </div>
                     </div>
                   </div>
@@ -18,7 +18,7 @@
                     <h3 class="card-title text-sm-center text-md-left vertical-center">
                       <span style="font-size: 1.5rem;">
                         <i class="far fa-clock"></i>
-                      </span> {{ time }}
+                      </span>
                     </h3>
                   </div>
                 </div>
@@ -27,34 +27,7 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-6">
-            <div class="card border-primary mb-3">
-              <div class="card-header text-center">
-                <h3><router-link to="/equipamento" class="text-dark">Equipamentos</router-link></h3>
-              </div>
-              <div class="card-body text-primary">
-                <h5 class="card-title">Quebrados: <span class="text-dark">{{ equipamentosQuebradosLength }}</span></h5>
-                <br>
-                <h5 class="card-title">Em Manutenção: <span class="text-dark">{{ equipamentosManutencaoLength }}</span></h5>
-                <br>
-                <h5 class="card-title">Reservas confirmadas: <span class="text-dark">{{ reservasEquipConfirmadasLength }}</span> </h5>
-                <br>
-                <h5 class="card-title">Reservas pendentes: <span class="text-dark">{{ reservasEquipPendentesLength }}</span></h5>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card border-primary mb-3">
-              <div class="card-header text-center">
-                <h3><router-link to="/equipamento" class="text-dark">Locais</router-link></h3>
-              </div>
-              <div class="card-body text-primary">
-                <h5 class="card-title">Reservas confirmadas: <span class="text-dark">{{ reservasLocalConfirmadasLength }}</span></h5>
-                <br>
-                <h5 class="card-title">Reservas pendentes: <span class="text-dark">{{ reservasLocalConfirmadasLength }}</span></h5>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
 </template>
@@ -63,15 +36,7 @@
 export default {
   name: 'ResumoComum',
   props: {
-    month: {type: String, default: 'Jan', required: true},
-    day: {type: Number, default: 1, required: true},
-    time: {type: Number, default: 2018, required: true},
-    reservasEquipConfirmadasLength: {type: Number, default: 0, required: true},
-    reservasEquipPendentesLength: {type: Number, default: 0, required: true},
-    equipamentosQuebradosLength: {type: Number, default: 0, required: true},
-    equipamentosManutencaoLength: {type: Number, default: 0, required: true},
-    reservasLocalConfirmadasLength: {type: Number, default: 0, required: true},
-    reservasLocalPendentesLength: {type: Number, default: 0, required: true}
+    month: {type: String, default: 'Jan', required: true}
   }
 }
 </script>
