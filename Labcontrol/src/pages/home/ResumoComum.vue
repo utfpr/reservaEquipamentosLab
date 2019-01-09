591e7f46b62fd623fac0225f417629710871e2b0
vitorCamargo <vitorcamargo@alunos.utfpr.edu.br>
2019-01-09 19:12:06 -0200

    Update ResumoComum.vue

--- Labcontrol/src/pages/home/ResumoComum.vue
+++ Labcontrol/src/pages/home/ResumoComum.vue
@@ -1,177 +1,329 @@
 <template>
-      <div class="container">
-        <a-row>  
-          <a-col :span="8" type = "flex" justify = "space-around" align = "middle">
-            <h1 align="center" >Calendar</h1>
-            <br/>
-            <div :style="{ width: '300px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
-              <a-row> 
-                <a-col :span="6" type = "flex" justify = "space-around" align = "middle"> 
-                  <a-icon 
-                    class = "iterativeIcons"
-                    type = "left"
-                    theme = "outlined"
-                    v-on:click= "onChangePanel('backward')"
-                  />
-                </a-col>
-                <a-col :span="12" type = "flex" justify = "space-around" align = "middle"> 
-                  {{this.data}}
-                </a-col>
-                <a-col :span="6" type = "flex" justify = "space-around" align = "middle"> 
-                  <a-icon 
-                    class = "iterativeIcons"
-                    type = "right"
-                    theme = "outlined"
-                    v-on:click= "onChangePanel('forward')"
-                  />
-                </a-col>
-              </a-row>
-                <div :style="{ width: '300px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
-                  <a-calendar :fullscreen="false" @select="onPanelChange" />
-                </div>
-            </div>
-          </a-col>
-          <a-col :span="16" type="flex" justify = "space-around" align = "middle">
-            <a-col :span="16" :offset="6">
-              <a-tabs @change="callback" type="card">
-                <a-tab-pane tab="Reservas de Equipamentos" key="1">
-                
-                  <a-list size="small" bordered :dataSource="reservasEquip">
-                    <a-list-item slot="renderItem" slot-scope="item, index">{{item}}</a-list-item>
-                    <h3 slot="header">Reservas de Equipamentos</h3>
-                    <div slot="footer">Footer</div>
-                  </a-list>
-                </a-tab-pane>
-                <a-tab-pane tab="Reservas de Locais" key="2">
-                  <a-list size="small" bordered :dataSource="reservasLocais">
-                    <a-list-item slot="renderItem" slot-scope="item, index">{{item}}</a-list-item>
-                    <h3 slot="header">Reservas de Locais</h3>
-                    <div slot="footer">Footer</div>
-                  </a-list>
-                </a-tab-pane>
-                <a-tab-pane tab="Minhas reservas" key="3">
-                  <a-list size="small" bordered :dataSource="reservasUser">
-                    <a-list-item slot="renderItem" slot-scope="item, index">{{reservasUser}}</a-list-item>
-                    <h3 slot="header">Minhas Reservas</h3>
-                    <div slot="footer">Footer</div>
-                  </a-list>
-                </a-tab-pane>
-              </a-tabs>
+  <a-spin :spinning = "loading">
+    <a-row :gutter = "16">  
+      <a-col :span = "8" style = "margin-top: 56px;">
+        <a-card>
+          <a-row slot = "title" style = "text-align: center;"> 
+            <a-col :span = "4"> 
+              <a-icon class = "buttons-icons" type = "double-left" @click = "prevYear" />
+              <a-icon class = "buttons-icons" type = "left" @click = "prevMonth" />
             </a-col>
-          </a-col>
-        </a-row>
-      </div>
+
+            <a-col :span = "16"> 
+              <i> {{ this.data.format('DD/MM/YYYY') }} </i>
+            </a-col>
+
+            <a-col :span = "4"> 
+              <a-icon class = "buttons-icons" type = "right" @click = "nextMonth" />
+              <a-icon class = "buttons-icons" type = "double-right" @click = "nextYear" />
+            </a-col>
+          </a-row>
+
+          <a-calendar :value = "data" :fullscreen = "false" @change = "(date, dateString) => changeDate(date)" />
+        </a-card>
+      </a-col>
+    
+      <a-col :span = "16">
+        <a-tabs type = "card">
+          <a-tab-pane tab = "Reservas de Equipamentos" key = "1">
+            <a-list :locale = "{ emptyText: 'Sem Reservas neste dia' }" size = "small" bordered :dataSource = "reservasEquipamentos">
+              <a-list-item slot = "renderItem" slot-scope = "item">
+                <a-tooltip placement = "left">
+                  <template slot = "title">
+                    <span> equipamento </span>
+                  </template>
+
+                  <a-list-item-meta>
+                    <span slot = "description">
+                      <b> [{{ item.solicitante.ra }} - {{ item.solicitante.nome }}] </b>
+                      <i> Equipamento: {{ item.equipamento.nome }} </i>.
+                      Das {{ item.horaInicio }} até {{ item.horaFim }}
+                    </span>
+                    
+                    <a-avatar :size = "20" slot = "avatar" :style = "{ backgroundColor: '#00a2ae' }" />
+                  </a-list-item-meta>
+                </a-tooltip>
+              </a-list-item>
+            </a-list>
+          </a-tab-pane>
+
+          <a-tab-pane tab = "Reservas de Locais" key = "2">
+            <a-list :locale = "{ emptyText: 'Sem Reservas neste dia' }" size = "small" bordered :dataSource = "reservasLocais">
+              <a-list-item slot = "renderItem" slot-scope = "item">
+                <a-tooltip placement = "left">
+                  <template slot = "title">
+                    <span> local </span>
+                  </template>
+
+                  <a-list-item-meta>
+                    <span slot = "description">
+                      <b> [{{ item.solicitante.ra }} - {{ item.solicitante.nome }}] </b>
+                      <i> Local: {{ item.local }}. </i>
+                      Das {{ item.horaInicio }} até {{ item.horaFim }}
+                    </span>
+                    
+                    <a-avatar :size = "20" slot = "avatar" :style = "{ backgroundColor: '#7265e6' }" />
+                  </a-list-item-meta>
+                </a-tooltip>
+              </a-list-item>
+            </a-list>
+          </a-tab-pane>
+
+          <a-tab-pane tab = "Reservas de Aulas" key = "3">
+            <a-list :locale = "{ emptyText: 'Sem Reservas neste dia' }" size = "small" bordered :dataSource = "reservasAulas">
+              <a-list-item slot = "renderItem" slot-scope = "item">
+                <a-tooltip placement = "left">
+                  <template slot = "title">
+                    <span> aula </span>
+                  </template>
+
+                  <a-list-item-meta>
+                    <span slot = "description">
+                      <b> [{{ item.solicitante.ra }} - {{ item.solicitante.nome }}] </b>
+                      <i> Local da Aula: {{ item.local }}. </i>
+                      Das {{ item.horaInicio }} até {{ item.horaFim }}
+                    </span>
+                    
+                    <a-avatar :size = "20" slot = "avatar" :style = "{ backgroundColor: '#ffbf00' }" />
+                  </a-list-item-meta>
+                </a-tooltip>
+              </a-list-item>
+            </a-list>
+          </a-tab-pane>
+
+ 
+          <a-tab-pane key = "4">
+            <a-badge slot = "tab" :dot = "reservasMinhas.length > 0"> Minhas Reservas </a-badge>
+
+            <a-list :locale = "{ emptyText: 'Sem Reservas neste dia' }" size = "small" bordered :dataSource = "reservasMinhas">
+              <a-list-item slot = "renderItem" slot-scope = "item">
+                <a-tooltip placement = "left">
+                  <template slot = "title">
+                    <span> {{ item.tipo }} </span>
+                  </template>
+
+                  <a-list-item-meta>
+                    <span v-if = "item.tipo === 'equipamento'" slot = "description">
+                      <i> Equipamento: {{ item.equipamento.nome }} </i>.
+                      Das {{ item.horaInicio }} até {{ item.horaFim }}
+                    </span>
+
+                    <span v-else-if = "item.tipo === 'local'" slot = "description">
+                      <i> Local: {{ item.local }}. </i>
+                      Das {{ item.horaInicio }} até {{ item.horaFim }}
+                    </span>
+
+                    <span v-else slot = "description">
+                      <i> Local da Aula: {{ item.local }}. </i>
+                      Das {{ item.horaInicio }} até {{ item.horaFim }}
+                    </span>
+                    
+                    <a-avatar :size = "20" slot = "avatar" :style = "{ backgroundColor: item.tipo === 'equipamento' ? '#00a2ae' : item.tipo === 'local' ? '#7265e6' : '#ffbf00' }" />
+                  </a-list-item-meta>
+                </a-tooltip>
+              </a-list-item>
+            </a-list>
+          </a-tab-pane>
+        </a-tabs>
+      </a-col>
+    </a-row>
+  </a-spin>
 </template>
  
 <script>
-import moment from 'moment'
-import 'moment/locale/pt-br'
-import firebaseApp from '../../firebase-controller.js'
-// import {Icon} from 'antd'
+  import firebaseApp from '../../firebase-controller.js'
 
-const db = firebaseApp.database()
+  const db = firebaseApp.database()
+  const auth = firebaseApp.auth()
 
-export default {
-  data () {
-    return {
-      data: moment().hours(0).minutes(0).seconds(0).milliseconds(0),
-      mounth: moment().hours(0).minutes(0).seconds(0).milliseconds(0),
-      equipamentos: [],
-      locais: [],
-      aulas: [],
-      reservasEquip: [],
-      reservasLocais: [],
-      reservasUser: []
-    }
-  },
-  name: 'ResumoComum',
-
-  beforeMount: function () {
-    let _this = this
-    db.ref('Equipamentos').orderByKey().on('value', function (snapshot) {
-      _this.equipamentos = []
-      snapshot.forEach(function (item) {
-        _this.equipamentos.push({
-          'key': item.key,
-          'patrimonio': item.val().Patrimonio,
-          'nome': item.val().Nome,
-          'local': item.val().Local,
-          'status': item.val().Status
-        })
-      })
-      console.log('equipamentos', _this.equipamentos)
-      db.ref('Reservas/equipamentos').orderByChild('Status').equalTo('Confirmada').on('value', function (snapshot) {
-        _this.reservasEquip = []
-        snapshot.forEach(function (item) {
-          var local
-          var equipamento
-          var val
-          val = _this.equipamentos.map(function (e) { return e.key }).indexOf(item.val().Equipamento)
-          if (val === -1) {
-            local = 'Error'
-          } else {
-            local = _this.equipamentos[_this.equipamentos.map(function (e) { return e.key }).indexOf(item.val().Equipamento)].local
-            equipamento = _this.equipamentos[_this.equipamentos.map(function (e) { return e.key }).indexOf(item.val().Equipamento)].patrimonio
-          }
-          _this.reservasEquip.push({
-            'id': item.val().Equipamento,
-            'equipamento': equipamento,
-            'local': local,
-            'dataInicio': item.val().Inicio,
-            'dataFim': item.val().Fim,
-            'status': item.val().Status
-          })
-        })
-        console.log('banana', _this.reservasEquip)
-      })
-    })
-  },
-  methods: {
-
-    callback (key) {
-      console.log(key + this.reservasUser)
+  export default {
+    data () {
+      return {
+        loading: true,
+        data: this.$moment().hours(0).minutes(0).seconds(0).milliseconds(0),
+        usuario: auth.currentUser.uid,
+        equipamentos: [],
+        usuarios: [],
+        reservasEquipamentos: [],
+        reservasLocais: [],
+        reservasAulas: [],
+        reservasMinhas: []
+      }
     },
-
-    onPanelChange (value) {
-      var data = value.format('DD/MM/YYYY HH:MM')
+    beforeMount: function () {
       let _this = this
-      moment(data).format()
-      console.log('data: ', data)
-      // let _this = this'
-      // console.log('estou aqui', this.state.data)
-      db.ref('Reservas/equipamentos').OrderBy('Inicio').equalTo(data).on('value', function (snapshot) {
-        _this.equipamentos = []
+      _this.loading = true
+
+      db.ref('Usuarios').on('value', function (snapshot) {
+        _this.loading = true
+        _this.usuarios = []
+
         snapshot.forEach(function (item) {
-          _this.equipamentos.push({
-            'Equipamento': item.val().Equipamento,
-            'Inicio': item.val().Inicio,
-            'Fim': item.val().Fim
+          _this.usuarios.push({
+            'id': item.key,
+            'ra': item.val().RA,
+            'nome': item.val().Nome + ' ' + item.val().Sobrenome
           })
         })
-        console.log('Este é o equipamento: ', _this.equipamentos)
+
+        db.ref('Equipamentos').on('value', function (snapshot) {
+          _this.loading = true
+          _this.equipamentos = []
+
+          snapshot.forEach(function (item) {
+            _this.equipamentos.push({
+              'id': item.key,
+              'patrimonio': item.val().Patrimonio,
+              'nome': item.val().Nome,
+              'status': item.val().Status
+            })
+          })
+
+          _this.atualizaReservas()
+          _this.loading = false
+        })
       })
+    },
+    methods: {
+      prevMonth () {
+        let _this = this
+
+        this.data = this.$moment(_this.data.add(-1, 'month'))
+        this.atualizaReservas()
+      },
+      prevYear () {
+        let _this = this
+
+        this.data = this.$moment(_this.data.add(-1, 'year'))
+        this.atualizaReservas()
+      },
+      nextMonth () {
+        let _this = this
+
+        this.data = this.$moment(_this.data.add(1, 'month'))
+        this.atualizaReservas()
+      },
+      nextYear () {
+        let _this = this
+
+        this.data = this.$moment(_this.data.add(1, 'year'))
+        this.atualizaReservas()
+      },
+      changeDate (date) {
+        this.data = date
+        this.atualizaReservas()
+      },
+      atualizaReservas () {
+        let _this = this
+        var dia = _this.$moment(_this.data.format('DD/MM/YYYY'), 'DD/MM/YYYY')
+        _this.reservasEquipamentos = []
+        _this.reservasLocais = []
+        _this.reservasAulas = []
+        _this.reservasMinhas = []
+
+        db.ref('Reservas/equipamentos').orderByChild('Status').equalTo('Confirmada').on('value', function (snapshot) {
+          snapshot.forEach(function (item) {
+            var inicio = _this.$moment(_this.$moment(item.val().Inicio, 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY'), 'DD/MM/YYYY')
+            var horaInicio = _this.$moment(_this.$moment(item.val().Inicio, 'DD/MM/YYYY HH:mm').format('HH:mm'), 'HH:mm')
+            var fim = _this.$moment(_this.$moment(item.val().Fim, 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY'), 'DD/MM/YYYY')
+            var horaFim = _this.$moment(_this.$moment(item.val().Fim, 'DD/MM/YYYY HH:mm').format('HH:mm'), 'HH:mm')
+
+            if (dia.isSameOrBefore(fim) && dia.isSameOrAfter(inicio)) {
+              if (item.val().Solicitante === _this.usuario) {
+                _this.reservasMinhas.push({
+                  'tipo': 'equipamento',
+                  'horaInicio': dia.isSame(inicio) ? horaInicio.format('HH:mm') : '07:00',
+                  'horaFim': dia.isSame(fim) ? horaFim.format('HH:mm') : '23:00',
+                  'equipamento': _this.equipamentos[_this.equipamentos.map(function (e) { return e.id }).indexOf(item.val().Equipamento)]
+                })
+              }
+
+              _this.reservasEquipamentos.push({
+                'horaInicio': dia.isSame(inicio) ? horaInicio.format('HH:mm') : '07:00',
+                'horaFim': dia.isSame(fim) ? horaFim.format('HH:mm') : '23:00',
+                'equipamento': _this.equipamentos[_this.equipamentos.map(function (e) { return e.id }).indexOf(item.val().Equipamento)],
+                'solicitante': _this.usuarios[_this.usuarios.map(function (e) { return e.id }).indexOf(item.val().Solicitante)]
+              })
+            }
+          })
+        })
+
+        db.ref('Reservas/locais').orderByChild('Status').equalTo('Confirmada').on('value', function (snapshot) {
+          snapshot.forEach(function (item) {
+            var inicio = _this.$moment(_this.$moment(item.val().Inicio, 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY'), 'DD/MM/YYYY')
+            var horaInicio = _this.$moment(_this.$moment(item.val().Inicio, 'DD/MM/YYYY HH:mm').format('HH:mm'), 'HH:mm')
+            var fim = _this.$moment(_this.$moment(item.val().Fim, 'DD/MM/YYYY HH:mm').format('DD/MM/YYYY'), 'DD/MM/YYYY')
+            var horaFim = _this.$moment(_this.$moment(item.val().Fim, 'DD/MM/YYYY HH:mm').format('HH:mm'), 'HH:mm')
+
+            if (dia.isSameOrBefore(fim) && dia.isSameOrAfter(inicio)) {
+              if (item.val().Solicitante === _this.usuario) {
+                _this.reservasMinhas.push({
+                  'tipo': 'local',
+                  'horaInicio': dia.isSame(inicio) ? horaInicio.format('HH:mm') : '07:00',
+                  'horaFim': dia.isSame(fim) ? horaFim.format('HH:mm') : '23:00',
+                  'local': item.val().Local
+                })
+              }
+
+              _this.reservasLocais.push({
+                'tipo': 'local',
+                'horaInicio': dia.isSame(inicio) ? horaInicio.format('HH:mm') : '07:00',
+                'horaFim': dia.isSame(fim) ? horaFim.format('HH:mm') : '23:00',
+                'local': item.val().Local,
+                'solicitante': _this.usuarios[_this.usuarios.map(function (e) { return e.id }).indexOf(item.val().Solicitante)]
+              })
+            }
+          })
+        })
+
+        db.ref('Reservas/aulas').orderByChild('Status').equalTo('Confirmada').on('value', function (snapshot) {
+          snapshot.forEach(function (item) {
+            var inicio = _this.$moment(item.val().Inicio, 'DD/MM/YYYY')
+            var fim = _this.$moment(item.val().Fim, 'DD/MM/YYYY')
+
+            if (dia.isSameOrBefore(fim) && dia.isSameOrAfter(inicio) && dia.day() === item.val().diaSemana) {
+              if (item.val().Solicitante === _this.usuario) {
+                _this.reservasMinhas.push({
+                  'tipo': 'aula',
+                  'horaInicio': item.val().horaInicio,
+                  'horaFim': item.val().horaFim,
+                  'local': item.val().Local
+                })
+              }
+
+              _this.reservasAulas.push({
+                'horaInicio': item.val().horaInicio,
+                'horaFim': item.val().horaFim,
+                'local': item.val().Local,
+                'solicitante': _this.usuarios[_this.usuarios.map(function (e) { return e.id }).indexOf(item.val().Solicitante)]
+              })
+            }
+          })
+        })
+      }
     }
   }
-
-}
 </script>
-<style>
-  .ant-row{
-    margin-Top: 5%;
-    margin-Bottom: 5%;
 
-  }
-  .ant-fullcalendar-header{
-    display:none;
-  }
-  .ant-select-selection--single{
-    display:none;
-  }
-  .ant-radio-group{
-    display:none;
-  }
-  .iterativeIcons:hover {
-    color:#1890ff;
+<style>
+  .buttons-icons {
     cursor: pointer;
+  }
+
+  .buttons-icons:hover {
+    color: #1890ff;
+  }
+
+  .ant-fullcalendar-header {
+    display: none;
+  }
+
+  .ant-fullcalendar.ant-fullcalendar-full {
+    border: none
+  }
+
+  .ant-list.ant-list-split .ant-spin-nested-loading {
+    overflow-y: scroll;
+    height: 50vh;
   }
 </style>
