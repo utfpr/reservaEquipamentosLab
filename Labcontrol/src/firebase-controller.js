6259c77da3e6e96e1336e83f3bdc4d033c0c7fa5
MathBatistela <matheusbatistela@alunos.utfpr.com.br>
2019-11-18 16:48:52 -0200

    firebase config single quote

--- Labcontrol/src/firebase-controller.js
+++ Labcontrol/src/firebase-controller.js
@@ -2,14 +2,14 @@ import firebase from 'firebase'
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyDBsU8g_Qo_TolceLqnw2LOdpM6qjBlcjA",
-  authDomain: "test-app-reserva.firebaseapp.com",
-  databaseURL: "https://test-app-reserva.firebaseio.com",
-  projectId: "test-app-reserva",
-  storageBucket: "test-app-reserva.appspot.com",
-  messagingSenderId: "304574833448",
-  appId: "1:304574833448:web:d2d9aae65895dffcf7261d",
-  measurementId: "G-JV7CRXK6QD"
+  apiKey: 'AIzaSyDBsU8g_Qo_TolceLqnw2LOdpM6qjBlcjA',
+  authDomain: 'test-app-reserva.firebaseapp.com',
+  databaseURL: 'https://test-app-reserva.firebaseio.com',
+  projectId: 'test-app-reserva',
+  storageBucket: 'test-app-reserva.appspot.com',
+  messagingSenderId: '304574833448',
+  appId: '1:304574833448:web:d2d9aae65895dffcf7261d',
+  measurementId: 'G-JV7CRXK6QD'
 }
 
 const firebaseApp = firebase.initializeApp(config)
